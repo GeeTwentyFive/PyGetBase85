bdb1b3db5325278a804d0e55c7a370c7012b89fe
Gee <alickoamarko@gmail.com>
2025-11-02 23:47:07 +0100

    Initial commit

---
+++ PyGetBase85.py
@@ -0,0 +1,15 @@
+import sys
+import base64
+
+
+if len(sys.argv) != 2:
+    print("USAGE: PyGetBase85 <TARGET_FILE_PATH>")
+    exit(1)
+
+
+data = ""
+with open(sys.argv[1], "rb") as f:
+    data = f.read()
+with open(sys.argv[1]+"_Base85.txt", "w") as out:
+    out.write(base64.a85encode(data).decode("ascii"))
+
